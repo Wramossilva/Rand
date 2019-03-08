d69fabf0e785f035e3a1eedc732ba1d7e3d0038b
RAMOS-SILVA <wilfried.ramossilva@gmail.com>
2019-03-08 15:27:30 +0100

    version1

---
+++ rand_0_1000.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$nb = 0;
+$valeur = rand(0, 1000) . "\n";
+$i = 1;
+
+while ( $valeur != $nb){
+    
+    $nb = rand(0,1000);
+   $i ++;
+   
+            
+}
+    
+
+echo  "le nombre de tour est " . $i  . " "."pour cette valeur de " . $valeur ;
+
+
+  
+?>
+
+
+
+
+
+
+
+
+
